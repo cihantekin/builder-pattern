44a3f312f76d70d8f25233664d6d826bf0f2ef64
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-12-25 18:14:07 +0100

    Add project files.

---
+++ builder-pattern/Abstract/IBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace builder_pattern.Abstract
+{
+    public interface IWatchBuilder
+    {
+        void BuildWatchSize();
+        void BuildWatchType();
+        void BuildWatchColor();
+        void BuildWatchStrap();
+        void BuildWatchStrapColor();
+    }
+}
