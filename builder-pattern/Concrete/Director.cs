023a06c262d157c519cf840be747437f56d20b3d
KTECH\Cihan <c.tekin@keesingtechnologies.com>
2022-12-27 20:18:03 +0100

    Director usage

---
+++ builder-pattern/Concrete/Director.cs
@@ -0,0 +1,17 @@
+﻿using builder_pattern.Abstract;
+
+namespace builder_pattern.Concrete
+{
+    public class Director
+    {
+        private IWatchBuilder _watchBuilder;
+        private IStrapBuilder _strapBuilder;
+
+        public IWatchBuilder WatchBuilder { set { _watchBuilder = value; } }
+        public IStrapBuilder StrapBuilder { set { _strapBuilder = value; } }
+
+        public void BuildRedAppleWatch() {
+            _watchBuilder.BuildWatchColor();
+        }
+    }
+}
